f2dd5b066a9e5154cef19b235492cfc09698be5a
wangsuwen <1002853070@qq.com>
2024-03-28 14:46:02 +0800

    添加：Redis消息队列Bull-UI

---
+++ 基于Redis的消息队列Bull-UI/pdfLib.js
@@ -0,0 +1,6 @@
+const PDFDocument = require('pdf-lib').PDFDocument;
+// import { PDFDocument } from "pdf-lib/cjs";
+// const Queue = require('bull');
+// console.log(Queue);
+const pdfDoc = PDFDocument.create();
+console.log(pdfDoc);
